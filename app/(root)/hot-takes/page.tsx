37e1aa798114bd0f932ad6af5b0816825cb4e589
evodata123 <evodata.info@gmail.com>
2024-11-17 15:33:12 +0530

    test:

--- app/(root)/hot-takes/page.tsx
+++ app/(root)/hot-takes/page.tsx
@@ -32,27 +32,24 @@ export default async function News() {
 
   return (
     <div className="mx-auto flex gap-x-10 mt-4 mb-10">
-     <Button variant="outline">Button 1</Button>
-     <Button variant="outline">Button 2</Button>
-     <Button variant="outline">Button 3</Button>
-     <Button variant="outline">Button 4</Button>
-     <Button variant="outline">Button 5</Button>
+     
+     
      <Table>
-     <TableCaption>A list of your recent invoices.</TableCaption>
+     <TableCaption>All the HOT Takes Today!</TableCaption>
       <TableHeader>
       <TableRow>
-      <TableHead className="w-[100px]">Invoice</TableHead>
-      <TableHead>Status</TableHead>
-      <TableHead>Method</TableHead>
-      <TableHead className="text-right">Amount</TableHead>
+      <TableHead className="w-[100px]">Post</TableHead>
+      <TableHead>User</TableHead>
+      <TableHead>Likes</TableHead>
+      <TableHead className="text-right">Comments</TableHead>
       </TableRow>
       </TableHeader>
       <TableBody>
       <TableRow>
-      <TableCell className="font-medium">INV001</TableCell>
-      <TableCell>Paid</TableCell>
-      <TableCell>Credit Card</TableCell>
-      <TableCell className="text-right">$250.00</TableCell>
+      <TableCell className="font-medium">Reliance Stock Up!!!</TableCell>
+      <TableCell>Parth</TableCell>
+      <TableCell>5</TableCell>
+      <TableCell className="text-right">10</TableCell>
       </TableRow>
       </TableBody>
       </Table>
