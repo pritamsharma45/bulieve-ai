3d457ab0c3b241cd56f011347ba272004c48ebac
evodata123 <evodata.info@gmail.com>
2024-11-17 15:28:21 +0530

    test:

--- app/(root)/hot-takes/page.tsx
+++ app/(root)/hot-takes/page.tsx
@@ -2,6 +2,16 @@ import prisma from "@/app/lib/db";
 import {NewsCard} from "./components/newsCard";
 import { Button } from "@/components/ui/button"
 import { unstable_noStore as noStore } from "next/cache";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 
 async function getData() {
   noStore();
@@ -27,6 +37,25 @@ export default async function News() {
      <Button variant="outline">Button 3</Button>
      <Button variant="outline">Button 4</Button>
      <Button variant="outline">Button 5</Button>
+     <Table>
+     <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+      <TableRow>
+      <TableHead className="w-[100px]">Invoice</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Method</TableHead>
+      <TableHead className="text-right">Amount</TableHead>
+      </TableRow>
+      </TableHeader>
+      <TableBody>
+      <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+      </TableRow>
+      </TableBody>
+      </Table>
 
     </div>
   );
